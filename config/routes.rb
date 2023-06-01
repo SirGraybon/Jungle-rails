524dc9ec361d45ae26abadd44eee9ba1948a537d
SirGraybon <graydonritchie@gmail.com>
2023-06-01 17:33:43 -0400

    adds category management page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-
+ 
+  
   get 'about/index'
   get "/about", to: "about#index"
 
   root to: 'products#index'
 
+
+
   resources :products, only: [:index, :show]
-  resources :categories, only: [:show]
+  resources :categories, only: [:index, :show]
 
   resource :cart, only: [:show] do
     post   :add_item
@@ -18,6 +21,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
